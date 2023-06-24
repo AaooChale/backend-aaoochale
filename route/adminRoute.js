2f03d2227730d1034daa441422027aa560b82311
Kishan <117573714+AaooChale@users.noreply.github.com>
2023-06-24 15:47:05 +0530

    Update adminRoute.js

--- route/adminRoute.js
+++ route/adminRoute.js
@@ -7,7 +7,7 @@ const BlogController = require("../controller/BlogController");
 const Authenticate = require("../middleware/Authenticate");
 const { check,body, validationResult } = require('express-validator');
 
-router.post("/getAllUsers",adminController.getAllUsers);
+router.post("/getAllUsers", adminController.getAllUsers);
 router.post("/adminLogin",
 check('email').not().isEmpty().withMessage('Email is required!!'),
 check('password').not().isEmpty().withMessage('Password is required!!'),
@@ -23,7 +23,7 @@ check('password').not().isEmpty().withMessage('Password is required!!'),
         adminController.adminLogin(req,res,next);
     }
 });
-router.post("/mangeUserStatus",Authenticate,
+router.post("/mangeUserStatus",
 check('id').not().isEmpty().withMessage('id is required!!'),
 check('status').not().isEmpty().withMessage('status is required!!'),
 (req,res,next)=>{
@@ -38,15 +38,15 @@ check('status').not().isEmpty().withMessage('status is required!!'),
         adminController.mangeUserStatus(req,res,next);
     }
 });
-router.post("/UserAllDoc", Authenticate,adminController.UserAllDoc);
-router.get("/AllCreatedRides",Authenticate, adminController.AllCreatedRides);
-router.get("/AllBookedRides",Authenticate, adminController.AllBookedRides);
-router.get("/AllRating",Authenticate, adminController.AllRating);
-router.get("/AllReport",Authenticate, adminController.AllReport);
-router.get("/AllVehicle",Authenticate, adminController.AllVehicle);   
-router.get("/DashbaordReport",Authenticate, adminController.DashbaordReport);   
-router.get("/rideReport",Authenticate, adminController.rideReport);      
-router.post("/broadcastNotification",Authenticate,
+router.post("/UserAllDoc",adminController.UserAllDoc);
+router.get("/AllCreatedRides", adminController.AllCreatedRides);
+router.get("/AllBookedRides", adminController.AllBookedRides);
+router.get("/AllRating", adminController.AllRating);
+router.get("/AllReport", adminController.AllReport);
+router.get("/AllVehicle", adminController.AllVehicle);   
+router.get("/DashbaordReport", adminController.DashbaordReport);   
+router.get("/rideReport", adminController.rideReport);      
+router.post("/broadcastNotification",
 check('message').not().isEmpty().withMessage('message is required!!'),
 (req,res,next)=>{
     const errors = validationResult(req);
@@ -61,10 +61,10 @@ check('message').not().isEmpty().withMessage('message is required!!'),
     }
 });
 
-router.get("/broadcastMessages",Authenticate, adminController.broadcastMessages);  
-router.get("/transactionalReport",Authenticate, adminController.transactionalReport);  
+router.get("/broadcastMessages", adminController.broadcastMessages);  
+router.get("/transactionalReport", adminController.transactionalReport);  
 
-router.post("/blockUnblockUser",Authenticate,
+router.post("/blockUnblockUser",
 check('userId').not().isEmpty().withMessage('userId field is required!!'),
 check('profileStatus').not().isEmpty().withMessage('profileStatus field is required!!'),
 (req,res,next)=>{
@@ -80,7 +80,7 @@ check('profileStatus').not().isEmpty().withMessage('profileStatus field is requi
     }
 });
 
-router.post('/business_settings_save',Authenticate,
+router.post('/business_settings_save',
 check('type').not().isEmpty().withMessage("The Type field cannot be empty!!"),
 body('value').not().isEmpty().withMessage("The Value field cannot be empty!!"),
 (req, res, next) => {
@@ -98,7 +98,7 @@ body('value').not().isEmpty().withMessage("The Value field cannot be empty!!"),
 
 
 //business settings List
-router.post('/business_settings_get',Authenticate,
+router.post('/business_settings_get',
 check('type').not().isEmpty().withMessage("The Type field cannot be empty!!"),
 (req, res, next) => {
     const errors = validationResult(req);
@@ -115,12 +115,12 @@ check('type').not().isEmpty().withMessage("The Type field cannot be empty!!"),
 
 
 //business settings List
-router.post('/business_settings_get_all',Authenticate,(req, res,next) => { 
+router.post('/business_settings_get_all',(req, res,next) => { 
     adminController.businessSettingGetAll(req,res,next);
 });
 
 
-router.post('/uploadImage',Authenticate,(req, res,next) => { 
+router.post('/uploadImage',(req, res,next) => { 
     adminController.uploadImage(req,res,next);
 });
 
@@ -130,7 +130,7 @@ require('express-group-routes');
 // Faq Api Start  
 router.group("/faq", (router) => {
     //add Api
-    router.post("/add",Authenticate,
+    router.post("/add",
     check('question').not().isEmpty().withMessage("The Question field cannot be empty!!"),
     // .custom(value => {
     //     return Role.findOne({name: value})
@@ -157,14 +157,14 @@ router.group("/faq", (router) => {
     }); 
 
     // List Api
-    router.post("/",Authenticate,
+    router.post("/",
     (req,res,next)=>{
         FaqController.list(req,res,next);
     }); 
 
 
     //Category Edit Api
-    router.post("/edit",Authenticate,
+    router.post("/edit",
     check('id').not().isEmpty().withMessage("The Id field is required!!"),
     (req,res,next)=>{
         const errors = validationResult(req);
@@ -181,7 +181,7 @@ router.group("/faq", (router) => {
     
     
     //Update Api
-    router.post("/update",Authenticate,
+    router.post("/update",
     //check('question').not().isEmpty().withMessage("The Question field cannot be empty!!"),
     // .custom(value => {
     //     return Role.findOne({name: value})
@@ -209,7 +209,7 @@ router.group("/faq", (router) => {
 
 
     //Delete Api
-    router.post("/delete",Authenticate,
+    router.post("/delete",
     check('id').not().isEmpty().withMessage("The Id field is required!!"),
     (req,res,next)=>{
         const errors = validationResult(req);
@@ -231,7 +231,7 @@ router.group("/faq", (router) => {
 // Testimonial Api Start  
 router.group("/testimonial", (router) => {
     //add Api
-    router.post("/add",Authenticate,
+    router.post("/add",
     body('name').not().isEmpty().withMessage("The Name field cannot be empty!!"),
     body('designation').not().isEmpty().withMessage("The Designation field cannot be empty!!"),
     body('review').not().isEmpty().withMessage("The Review field cannot be empty!!"),
@@ -250,14 +250,14 @@ router.group("/testimonial", (router) => {
     }); 
 
     // List Api
-    router.post("/",Authenticate,
+    router.post("/",
     (req,res,next)=>{
         TestimonialController.list(req,res,next);
     }); 
 
 
     //Category Edit Api
-    router.post("/edit",Authenticate,
+    router.post("/edit",
     check('id').not().isEmpty().withMessage("The Id field is required!!"),
     (req,res,next)=>{
         const errors = validationResult(req);
@@ -274,7 +274,7 @@ router.group("/testimonial", (router) => {
     
     
     //Update Api
-    router.post("/update",Authenticate,
+    router.post("/update",
     check('id').not().isEmpty().withMessage("The id field cannot be empty!!"),
     body('name').not().isEmpty().withMessage("The Name field cannot be empty!!"),
     body('designation').not().isEmpty().withMessage("The Designation field cannot be empty!!"),
@@ -295,7 +295,7 @@ router.group("/testimonial", (router) => {
 
 
     //Delete Api
-    router.post("/delete",Authenticate,
+    router.post("/delete",
     check('id').not().isEmpty().withMessage("The Id field is required!!"),
     (req,res,next)=>{
         const errors = validationResult(req);
@@ -316,7 +316,7 @@ router.group("/testimonial", (router) => {
 // Blog Api Start  
 router.group("/blog", (router) => {
     //add Api
-    router.post("/add",Authenticate,
+    router.post("/add",
     body('title').not().isEmpty().withMessage("The Name field cannot be empty!!"),
     body('description').not().isEmpty().withMessage("The description field cannot be empty!!"),
     body('shortDescription').not().isEmpty().withMessage("The shortDescription field cannot be empty!!"),
@@ -335,14 +335,14 @@ router.group("/blog", (router) => {
     }); 
 
     // List Api
-    router.post("/",Authenticate,
+    router.post("/",
     (req,res,next)=>{
         BlogController.list(req,res,next);
     }); 
 
 
     //Category Edit Api
-    router.post("/edit",Authenticate,
+    router.post("/edit",
     check('id').not().isEmpty().withMessage("The Id field is required!!"),
     (req,res,next)=>{
         const errors = validationResult(req);
@@ -359,7 +359,7 @@ router.group("/blog", (router) => {
     
     
     //Update Api
-    router.post("/update",Authenticate,
+    router.post("/update",
     check('id').not().isEmpty().withMessage("The id field cannot be empty!!"),
     body('title').not().isEmpty().withMessage("The Name field cannot be empty!!"),
     body('description').not().isEmpty().withMessage("The description field cannot be empty!!"),
@@ -380,7 +380,7 @@ router.group("/blog", (router) => {
 
 
     //Delete Api
-    router.post("/delete",Authenticate,
+    router.post("/delete",
     check('id').not().isEmpty().withMessage("The Id field is required!!"),
     (req,res,next)=>{
         const errors = validationResult(req);
